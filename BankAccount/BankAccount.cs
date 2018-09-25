bfb834960a625760673cc61553bbecdca47b5c88
pablovillazon <p.villazon@gmail.com>
2018-09-24 23:13:27 -0400

    Limpiando el codigo. Modificaciones al codigo aplicando principios de Clean Code.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BankSystem
+{
+    public class BankAccount
+    {
+        private string customerName;
+
+        private double currentBalance;
+
+        private bool isFrozen = false;
+
+        public BankAccount(string customerName, double currentBalance)
+        {
+            this.customerName = customerName;
+            this.currentBalance = currentBalance;
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+        }
+
+        public double CurrentBalance
+        {
+            get { return currentBalance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (isFrozen)
+            {
+                Console.WriteLine("Cannot debit. Account is frozen");
+                return;
+            }
+
+            if (amount > currentBalance)
+            {
+                Console.WriteLine("Insuficient available amount, your current balance is: " + currentBalance);
+                return;
+            }
+
+            if (amount < 0)
+            {
+                Console.WriteLine("Invalid credit amount");
+                return;
+            }
+
+            currentBalance += amount;  
+        }
+
+        public void Credit(double amount)
+        {
+            if (isFrozen)
+            {
+                Console.WriteLine("Cannot Credit. Account is frozen");
+                return;
+            }
+
+            if (amount < 0)
+            {
+                Console.WriteLine("Invalid credit amount");
+                return;
+            }
+
+            currentBalance -= amount;
+        }
+
+        public void FreezeAccount()
+        {
+            isFrozen = true;
+        }
+
+        public void UnfreezeAccount()
+        {
+            isFrozen = false;
+        }
+    }
+}
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,90 +1,20 @@
 ﻿using System;
 
-namespace BankAccountNS
+namespace BankSystem
 {
-    /// <summary>   
-    /// Bank Account   
-    /// </summary>   
-    public class BankAccount
+    public class Program
     {
-        private string m_customerName;
-
-        private double m_balance;
-
-        private bool m_frozen = false;
-
-        private BankAccount()
-        {
-        }
-
-        public BankAccount(string customerName, double balance)
-        {
-            m_customerName = customerName;
-            m_balance = balance;
-        }
-
-        public string CustomerName
-        {
-            get { return m_customerName; }
-        }
-
-        public double Balance
-        {
-            get { return m_balance; }
-        }
-
-        public void Debit(double amount)
-        {
-            if (m_frozen)
-            {
-                throw new Exception("Account frozen");
-            }
-
-            if (amount > m_balance)
-            {
-                throw new ArgumentOutOfRangeException("amount");
-            }
-
-            if (amount < 0)
-            {
-                throw new ArgumentOutOfRangeException("amount");
-            }
-
-            m_balance += amount; // intentionally incorrect code  
-        }
-
-        public void Credit(double amount)
-        {
-            if (m_frozen)
-            {
-                throw new Exception("Account frozen");
-            }
-
-            if (amount < 0)
-            {
-                throw new ArgumentOutOfRangeException("amount");
-            }
-
-            m_balance += amount;
-        }
-
-        private void FreezeAccount()
-        {
-            m_frozen = true;
-        }
-
-        private void UnfreezeAccount()
-        {
-            m_frozen = false;
-        }
-
         public static void Main()
         {
-            BankAccount ba = new BankAccount("Carlos Rojas", 10.50);
+            BankAccount bankAccount = new BankAccount("Carlos Rojas", 10.50);
+            
+            bankAccount.FreezeAccount();
 
-            ba.Credit(5.77);
-            ba.Debit(11.22);
-            Console.WriteLine("Current balance is ${0}", ba.Balance);
+            bankAccount.Credit(5.77);
+            bankAccount.Debit(11.22);
+
+            Console.WriteLine("Current balance is ${0}", bankAccount.CurrentBalance);
+
             Console.ReadKey();
         }
 
